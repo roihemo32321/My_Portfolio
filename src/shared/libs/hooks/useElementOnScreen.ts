aea4ea616fd1d53e71daca4e90a7039cf6920e4c
Roie <212544688@idf.il>
2023-03-29 21:19:02 +0300

    added some comments for an important function called useElementOnScreen.

--- src/shared/libs/hooks/useElementOnScreen.ts
+++ src/shared/libs/hooks/useElementOnScreen.ts
@@ -1,27 +1,32 @@
 import { RefObject, useEffect, useState } from "react";
 
 export default function useElementOnScreen(ref: RefObject<Element> | null | undefined, rootMargin = "0px") {
-	const [isIntersecting, setIsIntersecting] = useState(true);
+	const [isIntersecting, setIsIntersecting] = useState(true); // State for checking if element is on the view.
 
 	useEffect(() => {
-		const refValue = ref?.current;
+		if (!ref?.current) {
+			return;
+		}
+
+		const refValue = ref.current; // Getting the element by using a ref.
+
 		const observer = new IntersectionObserver(
 			([entry]) => {
 				setIsIntersecting(entry.isIntersecting);
 			},
 			{ rootMargin },
-		);
+		); // IntersectionObserver is a callback function that can return you if an element is on the view.
 
-		if (ref?.current) {
-			observer.observe(ref.current);
+		if (refValue) {
+			observer.observe(refValue);
 		}
 
 		return () => {
 			if (refValue) {
-				observer.unobserve(refValue);
+				observer.unobserve(refValue); // unobserving is really important on unmount.
 			}
 		};
 	}, [ref, rootMargin]);
 
-	return isIntersecting;
+	return isIntersecting; // returning if the element is on our view.
 }
