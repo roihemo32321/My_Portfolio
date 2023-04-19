3e18037da1d158beb595abf1c43c0f73b45f89f0
Roie <212544688@idf.il>
2023-04-19 15:00:24 +0300

    added target_blank for links

--- src/frontend/components/contacts/singleSocialLink.tsx
+++ src/frontend/components/contacts/singleSocialLink.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export default function SingleSocialLink({ svg, title, link: hrefLink }: Props) {
 	return (
-		<Link href={hrefLink} className={styles["social-link-style"]} aria-label="contact-me">
+		<Link href={hrefLink} className={styles["social-link-style"]} aria-label="contact-me" target="_blank">
 			<div className={`${styles["title-style"]} ${styles[`${title}-title-background`]}`}>{title}</div>
 			<div className={`${styles["svg-container"]} ${styles[`${title}-svg-background`]}`}>
 				<div className={styles["svg-style"]}>{svg}</div>
